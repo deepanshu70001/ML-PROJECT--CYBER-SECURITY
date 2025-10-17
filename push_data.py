ce89f49bdd6406b91748e44db6234a21c7dafe4d
deepanshu70001 <deepanshugarg70001@gmail.com>
2025-10-17 23:35:15 +0530

    mongoDB connection

--- push_data.py
+++ push_data.py
@@ -1,15 +1,62 @@
+import os
+import sys
+import json
+import pymongo
+import certifi
+from dotenv import load_dotenv
+from networksecurity.exception.exception import CustomException
+from networksecurity.logging.logger import logging
+import pandas as pd
+import numpy as np
+from networksecurity.logging.logger import logging
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+load_dotenv()
+mongo_DB_URL=os.getenv("MONGO_DB_URL")
+print(mongo_DB_URL)
 
-uri = "mongodb+srv://deepanshugarg704:Deepanshu26@cluster0.tvgbh38.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+import certifi
+ca=certifi.where()
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+class network_data_extract():
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+    def csv_to_json(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def insert_data_mongo(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+
+            self.mongo_client=pymongo.MongoClient(mongo_DB_URL)
+            self.database = self.mongo_client[self.database]
+            
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            
+            return (len(self.records))
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    file_path="Network_Data\phisingData.csv"
+    database="ml_prpject"
+    collection="NetworkData"
+    obj=network_data_extract()
+    records=obj.csv_to_json(file_path=file_path)
+    no_of_record=obj.insert_data_mongo(records=records,database=database,collection=collection)
+    print(no_of_record)
